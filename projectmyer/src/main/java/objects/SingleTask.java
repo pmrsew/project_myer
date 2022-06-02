308f5e6b6820b127997bde0e1aaa0d7fb410857d
Paige <paige0011@gmail.com>
2022-06-01 19:03:41 -0500

    Initial commit, beginnings of objects

---
+++ projectmyer/src/main/java/objects/SingleTask.java
@@ -0,0 +1,68 @@
+package objects;
+
+import java.time.LocalDate;
+
+public class SingleTask {
+
+    //Instance Variables
+    private int taskId;
+    private String taskName;
+    private boolean isCompleted;
+    private boolean hasDueDate;
+    private LocalDate dueDate;
+
+    //Constructors
+    public SingleTask(){
+    }
+    public SingleTask(int taskId, String taskName, boolean isCompleted, boolean hasDueDate, LocalDate LocalDate) {
+        this.taskId = taskId;
+        this.taskName = taskName;
+        this.isCompleted = isCompleted;
+        this.hasDueDate = hasDueDate;
+        this.dueDate = dueDate;
+    }
+
+    //Methods
+
+
+    public void trackTask(){
+        //track task method
+        //used to add to history
+    }
+
+    //Getters Setters
+    public int getTaskId() {
+        return taskId;
+    }
+    public void setTaskId(int taskId) {
+        this.taskId = taskId;
+    }
+
+    public String getTaskName() {
+        return taskName;
+    }
+    public void setTaskName(String taskName) {
+        this.taskName = taskName;
+    }
+
+    public boolean isCompleted() {
+        return isCompleted;
+    }
+    public void setCompleted(boolean completed) {
+        isCompleted = completed;
+    }
+
+    public boolean isHasDueDate() {
+        return hasDueDate;
+    }
+    public void setHasDueDate(boolean hasDueDate) {
+        this.hasDueDate = hasDueDate;
+    }
+
+    public LocalDate getDueDate() {
+        return dueDate;
+    }
+    public void setDueDate(LocalDate dueDate) {
+        this.dueDate = dueDate;
+    }
+}
---
+++ projectmyer/src/main/java/objects/TaskPool.java
@@ -0,0 +1,78 @@
+package objects;
+
+import java.time.LocalDate;
+import java.time.LocalDateTime;
+import java.util.HashMap;
+import java.util.Map;
+
+public class TaskPool {
+
+    //Instance Variables
+    private HashMap<Integer,SingleTask> singleTaskList;
+    //percentage out of hundred
+    private int completionPercent;
+    private LocalDateTime  completionDateTime;
+
+    //Constructors
+    public TaskPool() {
+    }
+    public TaskPool(HashMap<Integer,SingleTask> singleTaskList, int completionPercent, LocalDateTime completionDateTime) {
+        this.singleTaskList = singleTaskList;
+        this.completionPercent = completionPercent;
+        this.completionDateTime = completionDateTime;
+    }
+
+    //Methods
+
+    //remove task
+    public void removeTask(int taskId){
+        for(Map.Entry<Integer,SingleTask> singleTask : singleTaskList.entrySet()) {
+            if(singleTask.getKey().equals(taskId)){
+                singleTaskList.remove(taskId);
+            }
+        }
+    }
+
+    //add one task
+    public void addSingleTask(SingleTask singleTask){
+        singleTaskList.put(singleTask.getTaskId(), singleTask);
+    }
+    //add many task
+    public void addSingleTask(HashMap<Integer,SingleTask> taskHashMap){
+        singleTaskList.putAll(taskHashMap);
+    }
+
+
+    //total amount of task
+    public int totalSingleTasks(){
+        return singleTaskList.size();
+    }
+
+
+    //Getters Setters
+    public HashMap<Integer,SingleTask> getSingleTaskList() {
+        return singleTaskList;
+    }
+    public void setSingleTaskList(HashMap<Integer,SingleTask> singleTaskList) {
+        this.singleTaskList = singleTaskList;
+    }
+
+    //acts as refresh to percentage with each method call
+    public int getCompletionPercent() {
+        int completedTasks = 0;
+        for(Map.Entry<Integer,SingleTask> singleTask : singleTaskList.entrySet()) {
+            if(singleTask.getValue().isCompleted()){
+                completedTasks++;
+            }
+        }
+        completionPercent = completedTasks/this.totalSingleTasks();
+        return completionPercent;
+    }
+
+    public LocalDateTime getCompletionDateTime() {
+        return completionDateTime;
+    }
+    public void setCompletionDateTime(LocalDateTime completionDateTime) {
+        this.completionDateTime = completionDateTime;
+    }
+}
